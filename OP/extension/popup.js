a3c64bbb2bf73e16e65e8beb3799339674343450
aniruddha.humane <aniruddha.humane@outlook.com>
2023-09-10 12:57:01 +0900

    remove redundant folder

--- OP/extension/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('apply').addEventListener('click', () => {
-    let syncTime = document.getElementById('syncTime').value;
-    if (!syncTime) {
-        syncTime = 15;
-    }
-    chrome.storage.local.set({ syncTime: syncTime }, () => {
-        console.log('Sync time is set to ', syncTime);
-    });
-});
